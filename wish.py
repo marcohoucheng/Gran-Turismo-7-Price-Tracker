136a406f7f9f87151b3ec069376c8b985288df40
Marco Lam <marcohoucheng@gmail.com>
2024-02-22 20:47:17 +0100

    Working implementation of the wish.py script.

--- wish.py
+++ wish.py
@@ -1,25 +1,70 @@
 import urllib.request, json, os, sys, datetime
 import pandas as pd
 
-# Build the car -> ID table
-cars = pd.read_csv("https://raw.githubusercontent.com/ddm999/gt7info/web-new/_data/db/cars.csv")
-makers = pd.read_csv("https://raw.githubusercontent.com/ddm999/gt7info/web-new/_data/db/maker.csv")
+def main():
+    # list of cars from the game, should be a user input
+    wish = [216,773,810,1565,1984,2167,3362]
 
-cars = cars.rename({'ID': 'id', 'ShortName': 'Model'}, axis='columns')
-cars['id'] = cars['id'].map(str)
-cars['Maker'] = cars['Maker'].map(str)
-cars.set_index('id', inplace=True)
+    wish = pd.DataFrame(wish).astype(str)
 
-makers = makers.rename({'ID': 'id', 'Name': 'Manufacturer', 'Country': 'Country Code'}, axis='columns')
-makers['id'] = makers['id'].map(str)
-makers['Country Code'] = makers['Country Code'].map(str)
-makers.set_index('id', inplace=True)
+    shops = pd.read_csv("./data/shop.csv")
+    shops['id'] = shops['id'].astype(str)
+    shops.set_index('id', inplace=True)
 
-tab = pd.merge(cars, makers, how='left', left_on='Maker', right_index=True)
-tab = tab[['Manufacturer', 'Model']].sort_values(by=['Manufacturer', 'Model'])
+    # Check if the cars are in the shops
+    wish_used = wish[wish.isin(shops[shops['used']].index)].dropna()
+    wish_legend = wish[wish.isin(shops[shops['legend']].index)].dropna()
 
-# Provide a search list for users to select (multiple cars)
-# Store the list of cars ID
-# Separate these IDs into 3 tables (Central, Legend, Used)
-# Get latest price, when it was last in shops, and estimate days
-# Create a markdown?
+    cars = pd.read_csv("./data/cars.csv")
+    cars['id'] = cars['id'].astype(str)
+    cars.set_index('id', inplace=True)
+
+    ## Legend
+
+    # load legend db
+    if not(os.path.exists("./data/legend_historic.csv")):
+        print("legend shop DB not found in the data folder.\nPlease run build.py to create a new DB.")
+        return 0
+
+    legend_db = pd.read_csv("./data/legend_historic.csv")
+    legend_db['id'] = legend_db['id'].astype(str)
+    legend_db.set_index('id', inplace=True)
+    cols = legend_db.columns.tolist()
+    legend_db[cols[3:-3]] = legend_db[cols[3:-3]].astype('Int64')
+
+    for car in range(wish_legend.size):
+        car_id = wish_legend[0].tolist()[car]
+        car_info = cars.loc[car_id]
+        prices = legend_db.loc[[car_id]].dropna(axis=1, how='all')
+        last_avail = prices.columns.tolist()[3]
+        if last_avail == datetime.date.today().strftime("%y-%m-%d"):
+            print(car_info[0] + " " + car_info[1], " is in the shop today at price", int(prices[last_avail]), "cr.")
+        else:
+            print(car_info[0] + " " + car_info[1], " was last in the legend shop on " + last_avail + " at price", int(prices[last_avail]), "cr.")
+
+
+    ## Used
+    # load used db
+    if not(os.path.exists("./data/used_historic.csv")):
+        print("legend shop DB not found in the data folder.\nPlease run build.py to create a new DB.")
+        return 0
+
+    used_db = pd.read_csv("./data/used_historic.csv")
+    used_db['id'] = used_db['id'].astype(str)
+    used_db.set_index('id', inplace=True)
+    cols = used_db.columns.tolist()
+    used_db[cols[3:-3]] = used_db[cols[3:-3]].astype('Int64')
+
+    for car in range(wish_used.size):
+        car_id = wish_used[0].tolist()[car]
+        car_info = cars.loc[car_id]
+        prices = used_db.loc[[car_id]].dropna(axis=1, how='all')
+        last_avail = prices.columns.tolist()[3]
+        if last_avail == datetime.date.today().strftime("%y-%m-%d"):
+            print(car_info[0] + " " + car_info[1], " is in the shop today at price", int(prices[last_avail]), "cr.")
+        else:
+            print(car_info[0] + " " + car_info[1], " was last in the used shop on " + last_avail + " at price", int(prices[last_avail]), "cr.")
+    return 0
+
+if __name__ == "__main__":
+   main()
