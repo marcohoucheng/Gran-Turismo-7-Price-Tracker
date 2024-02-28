795b17e74e4736212b6be990c8e4b5e1341d48f1
Marco Lam <marcohoucheng@gmail.com>
2024-02-28 13:52:32 +0100

    Added a prompt to show only new items.

--- today.py
+++ today.py
@@ -3,15 +3,20 @@
 import urllib.request, json, os, sys, datetime
 import pandas as pd
 
-
-
-def today(shop, data):
-
-    print("\nAvailable in", shop, "shop:\n")
+def today(shop, data, new_data_only):
 
     shop_data = pd.DataFrame.from_dict(data[shop]['cars'])
 
+    if new_data_only:
+        shop_data = shop_data[shop_data['new'] == 'True']
+    
     shop_data = shop_data.sort_values(by=['manufacturer', 'name'])
+
+    if len(shop_data) == 0:
+        print("\nNo new items available in", shop, "shop.\n")
+        return 0
+
+    print("\nAvailable in", shop, "shop:\n")
 
     for _, row in shop_data.iterrows():
         if row['state'] == 'soldout':
@@ -25,6 +30,11 @@ def today(shop, data):
 
 def main():
 
+    if len(sys.argv) == 1:
+        new_data_only = False
+    else:
+        new_data_only = sys.argv[1] == 'new'
+
     date_format = '%y-%m-%d'
 
     url = urllib.request.urlopen("https://ddm999.github.io/gt7info/data.json")
@@ -34,12 +44,12 @@ def main():
     timestamp_date = data['updatetimestamp'][2:10]
 
     if datetime.datetime.strptime(timestamp_date, date_format).date() < datetime.date.today():
-        print("Today's data is not available yet. The latest data is from", timestamp_date, "\n")
+        print("\nToday's data is not available yet. The latest data is from", timestamp_date, "\n")
     else:
-        print("Today's data is available.\n")
+        print("\nToday's data is available.\n")
 
-    today('legend', data)
-    today('used', data)
+    today('legend', data, new_data_only)
+    today('used', data, new_data_only)
 
 if __name__ == "__main__":
     main()
