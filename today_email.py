fef89914981b18d56a12f0a97e8ad5fc134955bb
Marco Lam <108731804+marcohoucheng@users.noreply.github.com>
2024-03-07 20:59:55 +0100

    Update today_email.py

--- today_email.py
+++ today_email.py
@@ -61,8 +61,8 @@ def main():
     import os, ssl
 
     # Step 4: Load secrets from .env
-    gmail_address = os.environ("GMAIL_ADDRESS")
-    gmail_password = os.environ("GMAIL_PASSWORD")
+    gmail_address = os.environ.get("GMAIL_ADDRESS")
+    gmail_password = os.environ.get("GMAIL_PASSWORD")
 
     # Step 5: Create the email
     email = EmailMessage()
