1fb03387a12310a21454f89687a864c42283929e
Marco Lam <108731804+marcohoucheng@users.noreply.github.com>
2024-03-13 00:31:54 +0100

    First implementation of Markdown

--- today_email.py
+++ today_email.py
@@ -1,14 +1,16 @@
 # Script to simply return the available items in shops today.
 
-import urllib.request, json, os, sys, datetime, smtplib
+import urllib.request, json, os, sys, ssl, datetime, smtplib, markdown
 import pandas as pd
+from dotenv import load_dotenv
+from email.message import EmailMessage
 
 
 def main():
 
     def today(shop, data, new_data_only):
 
-        nonlocal return_string
+        nonlocal return_str, md_str
 
         shop_data = pd.DataFrame.from_dict(data[shop]['cars'])
 
@@ -18,20 +20,25 @@ def main():
         shop_data = shop_data.sort_values(by=['manufacturer', 'name'])
 
         if len(shop_data) == 0:
-            return_string = return_string + "\nNo new items available in " + shop + " shop.\n"
+            md_str = md_str + "\nNo new items available in " + shop + " shop.\n"
+            return_str = return_str + "\nNo new items available in " + shop + " shop.\n"
             return 0
 
-        return_string = return_string + "\n\nAvailable in " + shop + " shop:\n"
+        md_str = md_str + "\n\n## " + shop + " shop:\n"
+        return_str = return_str + "\n\nAvailable in " + shop + " shop:\n"
 
         for _, row in shop_data.iterrows():
             if row['state'] == 'soldout':
                 continue
             elif row['state'] == 'limited':
-                return_string = return_string + "\n" + row['manufacturer'] + " " + row['name'] + " : " + str(row['credits']) + "cr. (Leaving Soon)"
+                md_str = md_str + "\n" + row['manufacturer'] + " " + row['name'] + " : " + str(row['credits']) + "cr. (Leaving Soon)"
+                return_str = return_str + "\n" + row['manufacturer'] + " " + row['name'] + " : " + str(row['credits']) + "cr. (Leaving Soon)"
             elif row['new'] == 'True':
-                return_string = return_string + "\n" + row['manufacturer'] + " " + row['name'] + " : " + str(row['credits']) + "cr. (New)"
+                md_str = md_str + "\n" + row['manufacturer'] + " " + row['name'] + " : " + str(row['credits']) + "cr. (New)"
+                return_str = return_str + "\n" + row['manufacturer'] + " " + row['name'] + " : " + str(row['credits']) + "cr. (New)"
             else:
-                return_string = return_string + "\n" + row['manufacturer'] + " " + row['name'] + " : "+ str(row['credits']) + "cr."
+                md_str = md_str + "\n" + row['manufacturer'] + " " + row['name'] + " : "+ str(row['credits']) + "cr."
+                return_str = return_str + "\n" + row['manufacturer'] + " " + row['name'] + " : "+ str(row['credits']) + "cr."
 
     if len(sys.argv) == 1:
         new_data_only = False
@@ -51,14 +58,12 @@ def main():
     # else:
     #     print("\nToday's data is available.\n")
 
-    return_string = "Gran Turismo 7 shops for " + timestamp_date + "\n\n"
+    md_str = "# Gran Turismo 7 shops for " + timestamp_date + "\n\n"
+
+    return_str = "Gran Turismo 7 shops for " + timestamp_date + "\n\n"
 
     today('legend', data, new_data_only)
     today('used', data, new_data_only)
-
-    from dotenv import load_dotenv
-    from email.message import EmailMessage
-    import os, ssl
 
     # Step 4: Load secrets from .env
     gmail_address = os.environ.get("GMAIL_ADDRESS")
@@ -74,6 +79,9 @@ def main():
     email["From"] = gmail_address
 
     # Add HTML content to the email
+    html = markdown.markdown(md_str)
+    email.add_alternative(html, subtype="html")
+    
     # email.add_alternative(f"""\
     # <html>
     # <head></head>
@@ -84,7 +92,7 @@ def main():
     # """, subtype="html")
 
     # Add plaintext alternative as fallback option
-    email.set_content(return_string)
+    email.set_content(return_str)
 
     # Step 6: Send the email to the newsletter subscribers
     subscriber_email_addresses = json.loads(recipients)
